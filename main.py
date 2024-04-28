d205ebde62366aae26008eedaf62d19ad2f73a3f
TanThanhLe <tanthanhcr95@gmail.com>
2024-04-28 17:07:48 +0700

    Introduction to the help() function

--- main.py
+++ main.py
@@ -1,2 +1,22 @@
 if __name__ == "__main__":
-  print("Python Function Docstrings")
+  # Introduction to the help() function
+
+  """
+  Python provides a built-in function called help that allows you to show the documentation of a function.
+
+  The following examples shows the documentation of the print() function:
+
+  help(print)
+
+  Output:
+
+  print(...)
+    print(value, ,,. sep = " ", end = "\n", file = sys.stdout, flush = False )
+
+    Prints the values to a stream, or to sys.stdout by default.
+    Optional keyword arguments:
+    file:   a file-like object (stream); default to the current sys.stdout.
+    sep:    string inserted between values, default a space.
+    end:    string appended after the last value, default a newline.
+    flush:  whether to forcibly flush the stream.
+  """
